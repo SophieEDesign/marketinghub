08656a8d546f8bcbe2da9a085094a4cdac60aa9e
Sophie Edgerley <sophieedesign@gmail.com>
2025-12-15 20:40:04 +0000

    Add React Grid Layout styles to globals.css and remove redundant imports from BlockRenderer.tsx

--- components/blocks/BlockRenderer.tsx
+++ components/blocks/BlockRenderer.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import { Responsive, WidthProvider } from 'react-grid-layout'
-import 'react-grid-layout/css/styles.css'
-import 'react-resizable/css/styles.css'
 import TextBlock from './TextBlock'
 import ImageBlock from './ImageBlock'
 import ChartBlock from './ChartBlock'
