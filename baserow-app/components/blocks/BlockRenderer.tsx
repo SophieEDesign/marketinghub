e3ae50c8c47c3d3b53a9e95568a20b62522e5768
Sophie Edgerley <sophieedesign@gmail.com>
2025-12-15 20:42:53 +0000

    Refactor: Remove redundant React Grid Layout styles from globals.css and update BlockRenderer imports for consistency

--- baserow-app/components/blocks/BlockRenderer.tsx
+++ baserow-app/components/blocks/BlockRenderer.tsx
@@ -2,7 +2,7 @@
 
 import { Responsive, WidthProvider } from "react-grid-layout"
 import "react-grid-layout/css/styles.css"
-import "react-grid-layout/css/resizable.css"
+import "react-resizable/css/styles.css"
 import TextBlock from "./TextBlock"
 import ImageBlock from "./ImageBlock"
 import ChartBlock from "./ChartBlock"
--- components/blocks/BlockRenderer.tsx
+++ components/blocks/BlockRenderer.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { Responsive, WidthProvider } from 'react-grid-layout'
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css'
 import TextBlock from './TextBlock'
 import ImageBlock from './ImageBlock'
 import ChartBlock from './ChartBlock'
